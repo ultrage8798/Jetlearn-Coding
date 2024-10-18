37bd211e42bf88ed70df9d71b60540368b900bd5
ultrage8798 <ultrasoundvikram@gmail.com>
2024-10-17 20:07:25 -0500

    Add files via upload

--- my game.py
+++ my game.py
@@ -1,4 +1,5 @@
 import pygame 
+import random
 
 pygame.init()
 
@@ -13,6 +14,8 @@ class Player(pygame.sprite.Sprite):
         self.image=pygame.image.load("dog.png")
         self.image=pygame.transform.scale(self.image,(70,100))
         self.rect=self.image.get_rect()
+        self.rect.x=WIDTH//2
+        self.rect.y=HEIGHT//2
 
     def update(self,pressed_keys):
         if pressed_keys[pygame.K_UP]:
@@ -33,7 +36,7 @@ class Player(pygame.sprite.Sprite):
         elif self.rect.bottom>=HEIGHT:
             self.rect.bottom=HEIGHT
 
-sprites=pygame.sprite.Group()
+#sprites=pygame.sprite.Group()
 
 class Treat(pygame.sprite.Sprite):
     def __init__ (self):
@@ -42,24 +45,40 @@ class Treat(pygame.sprite.Sprite):
         self.image=pygame.transform.scale(self.image,(35,50))
         self.rect=self.image.get_rect()
 
-        if self.rect.left<=0:
-            self.rect.left=0
-        elif self.rect.right>=WIDTH:
-            self.rect.right=WIDTH
-        if self .rect.top<=0:
-            self.rect.top=0
-        elif self.rect.bottom>=HEIGHT:
-            self.rect.bottom=HEIGHT
+        self.rect.x=random.randint(0,WIDTH-self.rect.width)
+        self.rect.y=random.randint(0,HEIGHT-self.rect.height)
 
-sprites=pygame.sprite.Group()
+    def respawn(self):
+        self.rect.x=random.randint(0,WIDTH-self.rect.width)
+        self.rect.y=random.randint(0,HEIGHT-self.rect.height)
+
+all_sprites=pygame.sprite.Group()
+
+treats=pygame.sprite.Group()
 
 def start_game():
     player=Player()
-    sprites.add(player)
+    all_sprites.add(player)
     treat=Treat()
-    sprites.add(treat)
+    treats.add(treat)
+    all_sprites.add(treat)
+    return player
 
-for event in pygame.event.get():
-    if event.type==pygame.QUIT:
-        pygame.quit()
-        exit(0)
+player=start_game()
+
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            exit(0)
+    pressed_keys=pygame.key.get_pressed()
+    all_sprites.update(pressed_keys)
+    if pygame.sprite.spritecollide(player,treats,False,pygame.sprite.collide_rect):
+        print("Collision detected.")
+        for treat in treats:
+            treat.respawn()
+    screen.fill((0,0,0))
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    pygame.time.delay(30)
