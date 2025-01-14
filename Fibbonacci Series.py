8b7d5713f7bb1ce9f76c4111faefb744e88954ba
ultrage8798 <ultrasoundvikram@gmail.com>
2025-01-13 19:56:36 -0600

    Add files via upload

--- Fibbonacci Series.py
+++ Fibbonacci Series.py
@@ -1,22 +1,10 @@
-TITLE="Fibonacci Series"
 
-def Fibonacci(number):
-    fib=[0,1]
-    for i in range(2,number):
-        fib.append(fib[i-1]+fib[i-2])
-    return fib
-print (Fibonacci(10))
-
-def factorial(number):
-    if number==0:
-        return 1
+def reverse_string(words):
+    if len(words)==0:
+        return ""
     else:
-        return number*factorial(number-1)
-print(factorial(10))
+        return words[-1]+reverse_string(words[:-1])
+    
+print(reverse_string("vikram"))
 
-def summing(number):
-    if number==0 or number==1:
-        return number
-    else:
-        return number+summing(number-1)
-print(summing(10))
+    
