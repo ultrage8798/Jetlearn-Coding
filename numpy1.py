5cfd9a8510e44fad003ef5c4544cce972e5a883c
ultrage8798 <ultrasoundvikram@gmail.com>
2025-05-19 19:59:52 -0500

    Add files via upload

---
+++ numpy1.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+LA = np.array([1,2,3,4,5,6,7,8])
+
+print(LA[0:4])
+#print(LA[:7])
+#print(LA[::2])
+#print(LA[::-1])
+
+#MA = np.array([[1,2,3],[4,5,6],[7,8,9]])
+#print(MA[0:1,0;1])
+
+#k=np.array([1,2,3,4,5,6,7,8])
+#EA = k[k % 2 == 0]
+#print(EA)
+
+SL = [1,2,3,4,5,6,7,8]
